576f7a28cba98c850648f2e5496c2cb5edbae0da
Fabian Blechschmidt <Fabian.Blech@gmx.de>
2012-10-29 16:52:41 +0100

    girlfriend cries for coffee and cake.
    a small update, readme and test follows :-)

--- app/code/community/Schrank/DeveloperDefaultConfig/sql/schrank_developerdefaultconfig_setup/mysql4-upgrade-0.1.2-0.1.3.php
+++ app/code/community/Schrank/DeveloperDefaultConfig/sql/schrank_developerdefaultconfig_setup/mysql4-upgrade-0.1.2-0.1.3.php
@@ -1,6 +1,10 @@
 <?php
 /**
- * Install script to set all admin notifications as read
+ * Set all admin notifications as read
+ *
+ * The notifications are updated after the login of an administrator,
+ * because of this we have to trigger the observer method manually to
+ * set the notifications afterwards as read.
  *
  * @author     Fabian Blechschmidt <blechschmidt@fabian-blechschmidt.de>
  * @category   Schrank
@@ -9,6 +13,9 @@
  * @link       https://github.com/Schrank/DefaultDeveloperConfig
  * @since      File available since Release 0.1.3
  */
+$observer = Mage::getModel('adminnotification/observer');
+$observer->preDispatch(new Varien_Event_Observer());
+
 $collection = Mage::getModel('adminnotification/inbox')->getCollection();
 foreach ($collection as $notification) {
     /* @var $notification Mage_AdminNotification_Model_Inbox */
